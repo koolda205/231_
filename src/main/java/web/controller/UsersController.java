54d1dd365f1183ac63664e2a9b8fc98f0f2b2b3a
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-01 23:03:17 +0300

    still work but i am going to edit again

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -26,6 +26,7 @@ public class UsersController {
     public String getUserList (Model model) {
 
         model.addAttribute("users", userService.getUserlist());
+//        model.addAttribute("users", new User());
 //        model.addAttribute("getUserList", userService.getUserlist());
 
         return "users";
@@ -65,48 +66,6 @@ public class UsersController {
 
         return "userdata";
     }
-
-
-
-//    @Autowired
-//    public UsersController(UserService userService) {
-//        this.userService = userService;
-//    }
-
-//    @GetMapping()
-//    public String printUsers(@RequestParam(value = "count", required = false) Long count,
-//                             ModelMap modelMap) {
-//
-//        modelMap.addAttribute("users", userService.getUserlist(count));
-//        return "users";
-//    }
-
-
-//    @GetMapping()
-//    public String printUsers(@RequestParam(value = "count", required = false) Long count,
-//                            ModelMap modelMap) {
-//
-//        modelMap.addAttribute("users", userService.getUserlist(count));
-//        return "users";
-//    }
-
-//    @GetMapping()
-//    public String addUsers(@RequestParam(value = "name", required = false) String name,
-//                           @RequestParam(value = "last_name", required = false) String lastName,
-//                           @RequestParam(value = "email", required = false) String email,
-//                           ModelMap modelMap) {
-//
-//        modelMap.addAttribute("users", userService.addUserInUserList(name, lastName, email));
-//        return "users";
-//    }
-
-//    @GetMapping()
-//    public String addUsers(@RequestParam(value = "user", required = false) User user,
-//                              ModelMap modelMap) {
-//
-//        modelMap.addAttribute("users", userService.add(user));
-//        return "users";
-//    }
 }
 
 
