7aeb8f97f93751f3c8c0654243e0021ffae2999f
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-01 20:49:43 +0300

    add all methods and need to do pages

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
@@ -28,10 +25,45 @@ public class UsersController {
     @RequestMapping(value = "users", method = RequestMethod.GET)
     public String getUserList (Model model) {
 
-//        model.addAttribute("users", new User());
         model.addAttribute("users", userService.getUserlist());
+//        model.addAttribute("getUserList", userService.getUserlist());
 
         return "users";
+    }
+
+    @RequestMapping(value = "/users/add", method = RequestMethod.POST)
+    public String add (@ModelAttribute("user") User user) {
+
+        if (user.getId() == 0) {
+                userService.add(user);
+        } else {
+                userService.update(user);
+        }
+        return "redirect:/users";
+    }
+
+    @RequestMapping("/remove/{id}")
+    public String removeUser (@PathVariable("id") Long id) {
+
+        userService.deleteUserByID(id);
+
+        return "redirect:/users";
+    }
+    @RequestMapping("/delete/{id}")
+    public String editUser (@PathVariable("id") Long id,
+                            Model model) {
+        model.addAttribute("users", userService.getUserByID(id));
+        model.addAttribute("getUserList", userService.getUserlist());
+
+        return "users";
+    }
+
+    @RequestMapping("/userdata/{id}")
+    public String userData (@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("users", userService.getUserByID(id));
+
+        return "userdata";
     }
 
 
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 public class User {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name")
