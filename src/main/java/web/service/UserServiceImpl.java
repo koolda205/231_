3d918e5b769a11d0ff1e479c7857f7585050b729
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-06 12:32:38 +0300

    06/06 second commit

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -9,6 +9,8 @@ import web.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -40,21 +42,6 @@ public class UserServiceImpl implements UserService {
 //        return users2;
 //    }
 
-//    @Transactional(readOnly = true)
-//    @Override
-//    public List<User> getUserlist(Long count) {
-//        List<User> users = userDao.getUsersList();
-//        List<User> users2 = new ArrayList<>();
-//        if (count == null || count <= 0 || count > users.size()) {
-//            return users;
-//        } else {
-//            for (int i = 0; i < count; i++) {
-//                users2.add(users.get(i));
-//            }
-//        }
-//        return users2;
-//    }
-
     @Transactional(readOnly = true)
     @Override
     public List<User> getUserlist(Long count) {
@@ -69,6 +56,8 @@ public class UserServiceImpl implements UserService {
         }
         return users2;
     }
+
+
 
     @Transactional
     @Override
