27ecb7b53a0b91558c15dbf938bd95ca48a3bcc0
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-06 20:48:39 +0300

    add saveUser - still dont works

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -21,11 +21,11 @@ public class User {
     public User() {
     }
 
-    public User(String name, String lastName, String email) {
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-    }
+//    public User(String name, String lastName, String email) {
+//        this.name = name;
+//        this.lastName = lastName;
+//        this.email = email;
+//    }
 
     public Long getId() {
         return id;
