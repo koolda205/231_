1370a7bdcff7e33180592f7145453eb319cbe5c3
Dmitrii Kulakov <koolda@yandex.ru>
2023-06-07 22:09:15 +0300

    start again

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,12 +1,9 @@
 package web.config;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
 import javax.servlet.Filter;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
